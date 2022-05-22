07709c69576e3ff32f683a1b2bd78d08889ab577
Niloy-Dey <niloydeyce@gmail.com>
2022-05-22 23:23:51 +0600

    added order database from order page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
 const express = require('express')
 const app = express()
 const cors = require('cors');
@@ -17,11 +17,44 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.otvqo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
+
+        await client.connect();
+        // console.log('database connected');
+        const toolsCollection = client.db('manufacturing-website').collection('tools');
+        const ordersCollection = client.db('manufacturing-website').collection('orderDetails');
+
+
+
+
+        /* get method for all tools data loading in UI  */
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query)
+            const tools = await cursor.toArray();
+            res.send(tools);
+
+        })
+
+        //  single data finding for showing 
+        app.get('/tool/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await toolsCollection.findOne(query);
+            res.send(product);
+        });
+
+        /* post method for orders details */
+        app.post('/orderDetails', async(req, res) =>{
+            const orders= req.body;
+            const newOrder = await ordersCollection.insertOne(orders);
+            res.send(newOrder);
+        })
+
 
     }
-    finally{
+    finally {
 
     }
 
@@ -31,8 +64,8 @@ run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('welcome manufacture website');
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`manufacture website listening on port ${port}`)
-  })
+})
