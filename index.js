448209561f4c88b8a4cf7b44ab2e0a131a469b9e
Niloy-Dey <niloydeyce@gmail.com>
2022-05-24 12:09:00 +0600

    create admin and user role

--- index.js
+++ index.js
@@ -128,19 +128,16 @@ async function run() {
     /* Make a admin from user  */
     app.put('/user/admin/:email', async (req, res) => {
         const email = req.params.email;
-        const requester = req.decoded.email;
-        const requesterAccount = await userCollection.findOne({ email: requester });
-        if (requesterAccount.role === 'admin') {
+        // const requester = req.decoded.email;
+        // const requesterAccount = await userCollection.findOne({ email: requester });
+        // if (requesterAccount.role === 'admin') {
           const filter = { email: email };
-          const updateDoc = { $set: { role: 'admin' }, };
+          const updateDoc = { $set: { role: 'admin' } };
           const result = await userCollection.updateOne(filter, updateDoc);
-          res.send({ result });
+          res.send( result );
         }
-        else {
-          res.status(403).send({ message: 'forbidden' })
-        }
-  
-      })
+    //   }
+      )
     }
     finally {
 
